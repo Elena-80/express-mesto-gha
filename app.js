9559a87f72194ada91f3621bdb592bdc63e7a19f
Elena-80 <elena.chumakova80@yandex.ru>
2023-08-05 10:31:50 +0300

    Error messages fixed

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const notFoundRouter = require('./errors/notFoundRouter');
-const helmet = require('helmet');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
