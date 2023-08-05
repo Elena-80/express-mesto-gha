67f619b16a8b91f617ed5e586d6c184d67d68d64
Elena-80 <elena.chumakova80@yandex.ru>
2023-08-05 20:59:45 +0300

    Error messages fixed

--- controllers/users.js
+++ controllers/users.js
@@ -7,11 +7,6 @@ module.exports.getUser = (req, res) => {
         res.status(404).send({ message: 'Нет пользователя с таким id' });
         return;
       }
-      // if (req.params.userId !== user._id.toString()) {
-      // Получение пользователя с некорректным id
-      //   res.status(400).send({ message: 'Ошибка при валидации' });
-      //   return;
-      // }
       res.status(200).send(user);
     })
     .catch((err) => {
@@ -39,7 +34,7 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => {
-      res.status(200).send(user);
+      res.status(201).send(user);
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
