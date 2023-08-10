db23d213a9042a847dd97c7509513f5157576259
Elena-80 <elena.chumakova80@yandex.ru>
2023-08-10 18:05:02 +0300

    Validation added

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const helmet = require('helmet');
 const bodyParser = require('body-parser');
 // const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const { celebrate, Joi } = require('celebrate');
 const handleError = require('./middlewares/handleError');
+
+const URL_PATTERN = /^https?:\/\/(?:w{3}\.)?(?:[a-z0-9]+[a-z0-9-]*\.)+[a-z]{2,}(?::[0-9]+)?(?:\/\S*)?#?$/i;
 
 const NotFoundError = require('./errors/NotFoundError');
 const {
@@ -29,8 +32,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.use(cookieParser());
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    name: Joi.string().default('Жак-Ив Кусто').min(2).max(30),
+    about: Joi.string().default('Исследователь').min(2).max(30),
+    avatar: Joi.string().regex(URL_PATTERN)
+      .default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'),
+  }),
+}), createUser);
 
 app.use(auth);
 app.use('/users', require('./routes/users'));
